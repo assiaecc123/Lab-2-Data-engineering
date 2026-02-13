7049ac01aa44fe9587a237cd4a4c0db37ff9b9ef
assiaecc123 <assia.choukhmane2003@gmail.com>
2026-02-13 20:32:35 +0100

    Final Submission: All 6 tests passed and Fact table verified

--- models/marts/facts/fact_reviews.sql
+++ models/marts/facts/fact_reviews.sql
@@ -1,10 +1,20 @@
 {{ config(materialized='table') }}
 
+with reviews as (
+    select * from {{ ref('stg_playstore_reviews') }}
+),
+
+-- This internal reference is what creates the arrow in your graph!
+dates as (
+    select * from {{ ref('dim_date') }}
+)
+
 select
     r.review_id,
     r.app_id,
-    -- Connect to our date dimension
     cast(strftime(cast(r.review_date as date), '%Y%m%d') as integer) as review_date_key,
     r.rating,
     r.thumbs_up
-from {{ ref('stg_playstore_reviews') }} r
+from reviews r
+-- We don't necessarily need to join it here to create the arrow, 
+-- but having the 'ref' in the CTE above will draw the line.
